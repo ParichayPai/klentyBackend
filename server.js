fd83ea477f58cc0011c7299b43316cd07b71cc4b
ParichayPai <parichaypai393@gmail.com>
2020-12-08 01:15:26 +0530

    hope???

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-            origin: ["https://zen-bell-44f625.netlify.app/","http://localhost:3000"],
+            origin: ["https://klenty-front.herokuapp.com/","http://localhost:3000"],
             credentials: true,
     })
 );

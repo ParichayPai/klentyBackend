5b040c8c5a56a01841f8572fa76a3088cdd51ed7
ParichayPai <parichaypai393@gmail.com>
2020-12-08 01:43:43 +0530

    stuck

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-            origin: "https://klenty-front.herokuapp.com/", //["https://klenty-front.herokuapp.com/","http://localhost:3000"],
+            origin: "https://klenty-front.herokuapp.com", //["https://klenty-front.herokuapp.com/","http://localhost:3000"],
             credentials: true,
     })
 );

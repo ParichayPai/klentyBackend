79dbb5e84756a20e569c8e29e3ec0d35d47d7f85
ParichayPai <parichaypai393@gmail.com>
2020-12-07 17:33:17 +0530

    asdasd

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
